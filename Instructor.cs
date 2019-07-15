9a61ffd22dae086bd6bcf6e791b9cbc8bbaa5039
Carly Bergthold <carlybergthold@gmail.com>
2019-07-15 11:21:47 -0500

    NSSPerson class added

--- Instructor.cs
+++ Instructor.cs
@@ -2,12 +2,8 @@ using System;
 
 namespace studentExercises
 {
-    public class Instructor
+    public class Instructor : NSSPerson
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string SlackHandle { get; set; }
-        public int Cohort { get; set; }
         public string Speciality { get; set; }
         public void AssignEx(Exercise ex, Student stu) {
             stu.AddEx(ex);
